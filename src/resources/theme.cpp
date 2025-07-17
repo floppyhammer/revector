eb0f975dc2838e8638891cd1dfc9bd4756f6f19f
floppyhammer <tannhauser_chen@outlook.com>
2025-07-17 23:46:28 +0800

    Fix system font loading on Windows

--- src/resources/theme.cpp
+++ src/resources/theme.cpp
@@ -10,7 +10,11 @@ extern "C" {
 namespace revector {
 
 void Theme::load_system_font() {
+#ifdef WIN32
+    auto font_buffer = find_system_font("SimHei");
+#else
     auto font_buffer = find_system_font("Droid Sans Fallback");
+#endif
 
     if (font_buffer.data != nullptr && font_buffer.len > 0) {
         auto font_data = std::vector<char>(font_buffer.data, font_buffer.data + font_buffer.len);
