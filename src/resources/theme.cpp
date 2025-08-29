d0838690ae4c531713e2d4cae231940433f1055d
floppyhammer <tannhauser_chen@outlook.com>
2025-08-30 00:06:48 +0800

    Improve font loading and system font searching

--- src/resources/theme.cpp
+++ src/resources/theme.cpp
@@ -10,20 +10,36 @@ extern "C" {
 namespace revector {
 
 void Theme::load_system_font() {
+    font = DefaultResource::get_singleton()->get_default_font();
+
 #ifdef WIN32
-    auto font_buffer = find_system_font("SimHei");
+    std::array<const char*, 3> font_priority{
+        "Microsoft YaHei",
+        "SimSun",
+        "SimHei",
+    };
 #else
-    auto font_buffer = find_system_font("Droid Sans Fallback");
+    std::array<const char*, 3> font_priority{
+        "Droid Sans Fallback",
+        "Noto Sans CJK",
+        "Noto Serif CJK",
+    };
 #endif
 
-    if (font_buffer.data != nullptr && font_buffer.len > 0) {
-        auto font_data = std::vector<char>(font_buffer.data, font_buffer.data + font_buffer.len);
+    FontBuffer font_buffer{};
+    for (const auto& font_name : font_priority) {
+        font_buffer = find_system_font(font_name);
+        if (font_buffer.data && font_buffer.len > 0) {
+            const auto font_data = std::vector<char>(font_buffer.data, font_buffer.data + font_buffer.len);
+            free_font_buffer(font_buffer);
 
-        font = std::make_shared<Font>(font_data);
-
-        free_font_buffer(font_buffer);
-    } else {
-        font = DefaultResource::get_singleton()->get_default_font();
+            // If the font is valid
+            if (const auto new_font = Font::from_memory(font_data)) {
+                font = new_font;
+                break;
+            }
+        }
+        Logger::warn("System font is not found/valid: " + std::string(font_name), "revector");
     }
 }
 
