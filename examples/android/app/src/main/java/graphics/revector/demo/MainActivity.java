d246b5df56f8b34c08adf5679de99a9e9fdc81da
floppyhammer <tannhauser_chen@outlook.com>
2025-10-14 14:53:17 +0800

    Hide navigation bar in Android example

--- examples/android/app/src/main/java/graphics/revector/demo/MainActivity.java
+++ examples/android/app/src/main/java/graphics/revector/demo/MainActivity.java
@@ -3,12 +3,16 @@ package graphics.revector.demo;
 import com.google.androidgamesdk.GameActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends GameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Hide navigation bar.
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 
     static {
